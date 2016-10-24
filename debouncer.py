ceca24e9c4f9e8ad5aaebc82161d1643386404c3
Andrey Neverov <neverov.and@gmail.com>
2016-10-24 18:44:42 +0300

    fixed find_filter method

--- debouncer.py
+++ debouncer.py
@@ -56,8 +56,8 @@ def build_service(credentials):
     return service
 
 @log
-def find_label(service):
-    result = service.users().labels().list(userId='me').execute()
+def find_label(user_id, service):
+    result = service.users().labels().list(userId=user_id).execute()
     labels = result['labels']
     label = next((l for l in labels if l['name'] == LABEL_NAME), None)
     if label is not None:
@@ -66,58 +66,61 @@ def find_label(service):
         return None
 
 @log
-def create_label(service):
+def create_label(user_id, service):
     body = {'name': LABEL_NAME,
             'labelListVisibility': 'labelShow',
             'messageListVisibility': 'show'}
-    result = service.users().labels().create(userId='me', body=body).execute()
+    result = service.users().labels().create(userId=user_id, body=body).execute()
     return result['id']
 
 @log
-def find_filter(service):
-    result = service.users().settings().filters().list(userId='me').execute()
+def find_filter(user_id, service):
+    result = service.users().settings().filters().list(userId=user_id).execute()
     if len(result) == 0:
         return None
     filters = result['filter']
     for f in filters:
-        exists = (f['action']['removeLabelIds'] == LABEL_IDS and
+        exists = ('action' in f and
+                  'removeLabelIds' in f['action'] and
+                  f['action']['removeLabelIds'] == LABEL_IDS and
+                  'criteria' in f and
                   f['criteria'] == CRITERIA)
         if exists:
             return f['id']
     return None
 
 @log
-def create_filter(labelId, service):
+def create_filter(user_id, labelId, service):
     body = {'criteria': CRITERIA,
             'action': {'removeLabelIds': LABEL_IDS,
                        'addLabelIds': [labelId]}}
-    result = service.users().settings().filters().create(userId='me', body=body).execute()
+    result = service.users().settings().filters().create(userId=user_id, body=body).execute()
     return result['id']
 
 @log
-def authorize(credentials):
+def authorize(user_id, credentials):
     logger.info('logging in to gmail')
     service = build_service(credentials)
 
     logger.info('checking special label in gmail')
-    label_id = find_label(service)
+    label_id = find_label(user_id, service)
     if label_id is None:
         logger.info('no special label found, creating one')
-        label_id = create_label(service)
+        label_id = create_label(user_id, service)
         logger.info('special label created')
     logger.info('special label exists, moving on')
 
     logger.info('checking special filter exists')
-    if find_filter(service) is None:
+    if find_filter(user_id, service) is None:
         logger.info('no special filter found, creating one')
-        create_filter(label_id, service)
+        create_filter(user_id, label_id, service)
         logger.info('special filter created')
     logger.info('special filter found, moving on')
     logger.info('user setup complete')
 
 @log
-def messages_for_label(label_id, service):
-    response = service.users().messages().list(userId='me',
+def messages_for_label(user_id, label_id, service):
+    response = service.users().messages().list(userId=user_id,
                                                labelIds=[label_id]).execute()
     messages = []
     if 'messages' in response:
@@ -133,34 +136,34 @@ def messages_for_label(label_id, service):
     return messages
 
 @log
-def run(user, delay, credentials):
-    logger.info('running debouncer for user: {}'.format(user))
-    debounced_messages = timers[user]['messages']
+def run(user_id, delay, credentials):
+    logger.info('running debouncer for user: {}'.format(user_id))
+    debounced_messages = timers[user_id]['messages']
     logger.info('searching for debounced messages')
     service = build_service(client.OAuth2Credentials.from_json(credentials))
-    label_id = find_label(service)
-    messages = messages_for_label(label_id, service)
-    body = {'addLabelIds': LABEL_IDS,
-            'removeLabelIds': [label_id]}
+    label_id = find_label(user_id, service)
+    messages = messages_for_label(user_id, label_id, service)
     messages_count = len(messages)
     if (debounced_messages != messages_count): # debounce to next cycle
-        logger.info('debouncing to the next cycle with prev counter: {}, switching to next counter: {}'.format(debounced_messages,
-                                                                                                         messages_count))
-        timers[user]['messages'] = messages_count
+        logger.info('debouncing: last counter: {}, next counter: {}'.format(debounced_messages,
+                                                                            messages_count))
+        timers[user_id]['messages'] = messages_count
     else: # no new messages, time to move everything to inbox
+        body = {'addLabelIds': LABEL_IDS,
+                'removeLabelIds': [label_id]}
         logger.info('moving {} messages to inbox'.format(messages_count))
         for m in messages:
             id = m['id']
-            service.users().messages().modify(userId='me',
+            service.users().messages().modify(userId=user_id,
                                               id=id,
                                               body=body).execute()
         logger.info('moved {} messages to inbox'.format(len(messages)))
-        timers[user]['messages'] = 0
-    logger.info('run for user: {} completed'.format(user))
+        timers[user_id]['messages'] = 0
+    logger.info('run for user: {} completed'.format(user_id))
     logger.info('restarting the timer')
-    timer = Timer(float(delay), run, [user, delay, credentials])
+    timer = Timer(float(delay), run, [user_id, delay, credentials])
     timer.start()
-    timers[user]['timer'] = timer
+    timers[user_id]['timer'] = timer
     logger.info('timer restarted')
 
 @app.route('/timer/start', methods=['POST'])
@@ -209,12 +212,12 @@ def index():
         logger.info('token expired, redirecting to auth')
         return flask.redirect(flask.url_for('oauth2callback'))
     else:
-        email = flask.session['email']
-        logger.info('logged in user %s', email)
-        authorize(credentials)
-        enabled = email in timers
+        user_id = flask.session['email']
+        logger.info('logged in user %s', user_id)
+        authorize(user_id, credentials)
+        enabled = user_id in timers
         delay = flask.session.get('delay', 300)
-        return flask.render_template('index.html', name=email, enabled=enabled, delay=delay)
+        return flask.render_template('index.html', name=user_id, enabled=enabled, delay=delay)
 
 @app.route('/oauth2callback')
 def oauth2callback():
